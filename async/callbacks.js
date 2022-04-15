ec0bfa9b3639c15bc843a29f159c5236f00fa1f4
Cesar Soberanis <cmendozasoberanis@gmail.com>
2022-04-14 23:31:34 -0500

    Como manejar la asincronia

--- async/callbacks.js
+++ async/callbacks.js
@@ -1,3 +1,44 @@
-function soyAsincrono() {
-  console.log('Hola, soy una funcion asincrona');
-}
+// Version ES
+const hola = ((nombre, miCallback) => {
+  setTimeout(() => {
+    console.log(`Hola ${nombre}`);
+    miCallback();
+  }, 1000);
+});
+
+const adios = ((nombre, otroCallback) => {
+  setTimeout(() => {
+    console.log(`Adios ${nombre}`);
+    otroCallback();
+  }, 1000);
+});
+
+console.log(`Iniciando proceso ...`);
+hola('Cesar', () => {
+  adios('Cesar', () => {
+    console.log('Terminando proceso ...');
+  })
+});
+
+
+// function hola(nombre, miCallback) {
+//   // console.log('Hola, soy una funcion asincrona');
+//   setTimeout(() => {
+//     console.log(`Hola ${nombre}`);
+//     miCallback();
+//   }, 1000);
+// }
+
+// function adios(nombre, otroCallback) {
+//   setTimeout(() => {
+//     console.log(`Adios ${nombre}`);
+//     otroCallback()
+//   }, 1000);
+// }
+
+// console.log(`Iniciando proceso ...`);
+// hola('Cesar', function () {
+//   adios('Cesar', function () {
+//     console.log('Terminando proceso ...');
+//   });
+// });
