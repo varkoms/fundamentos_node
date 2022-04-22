0f012a26d2d6f004212e3145bba393d87d9faaed
Cesar Soberanis <cmendozasoberanis@gmail.com>
2022-04-21 23:22:16 -0500

    Borrando runner temporal

--- memoria/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-// Guardar el abecedario en un buffer
-buffer = Buffer.alloc(26);
-for (let i = 0; i < buffer.length; i++) {
-  buffer[i] = i + 97;  
-}
-
-console.log(buffer);
-console.log(buffer.toString());
